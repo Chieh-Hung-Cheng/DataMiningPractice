cf0fcdb9e33b72b0ddf707b0c912aa1dccd2a9a2
Chieh-Hung-Cheng <james110012@gmail.com>
2022-03-10 11:37:56 +0800

    Added Phrase Class

--- main.py
+++ main.py
@@ -60,22 +60,104 @@ def associations(lmttyp, df_ctr, df_all_ctr):
     ret_lft_lst = sorted(lft_dict.items(), key=lambda x: x[1], reverse=True)
     return ret_sup_lst, ret_cfd_lst, ret_lft_lst
 
-def main():
+class Phrase:
+    def __init__(self, name, tf_up, df_up, tf_down, df_down):
+        self.name = name
+        # Frequencies
+        self.tf_up = tf_up
+        self.df_up = df_up
+        self.tf_down = tf_down
+        self.df_down = df_down
+        self.df_all = self.df_up + self.df_down # df_all
+
+        # MI, tfidf, associations
+        # UP
+        self.MI_up = 0
+        self.tfidf_up = 0
+        self.supp_up = 0
+        self.conf_up = 0
+        self.lift_up = 0
+        # Down
+        self.MI_down = 0
+        self.tfidf_down = 0
+        self.supp_down = 0
+        self.conf_down = 0
+        self.lift_down = 0
+        for elm in ['up', 'down']:
+            self.calc_MI(elm)
+            self.calc_tfidf(elm)
+            self.calc_assocs(elm)
+
+    def calc_MI(self, lmttyp):
+        # MI = log(N(XY) / (N(X)N(Y)), belongs to df
+        if lmttyp == 'up':
+            self.MI_up = math.log(self.df_up + 1e-4 / (N_up*self.df_all))
+        elif lmttyp == 'down':
+            self.MI_down = math.log(self.df_down + 1e-4 / (N_down*self.df_all))
+
+    def calc_tfidf(self, lmttyp):
+        # tf-idf = (1+log(tf)) * log(N_ttl/df) ???
+        N_tmp = N_up if lmttyp == 'up' else N_down
+        if lmttyp == 'up':
+            self.tfidf_up = (1+math.log(self.tf_up+1e-4)) * math.log(N_ttl/self.df_all)
+        elif lmttyp == 'down':
+            self.tfidf_down = (1+math.log(self.tf_down+1e-4)) * math.log(N_ttl/self.df_all)
+
+    def calc_assocs(self, lmttyp):
+        # Support P(XY) = N(XY)/N_ttl
+        # Confidence(X->Y) P(Y|X) = P(XY)/P(X) = N(XY)/N(X)
+        # Lift P(XY)/(P(X)P(Y)) = N_ttl*N(XY)/(N(X)N(Y))
+        # Feature (X) -> up or down (Y)
+        if lmttyp == 'up':
+            self.supp_up = self.df_up / N_ttl
+            self.conf_up = self.df_up / self.df_all
+            self.lift_up = (N_ttl * self.df_up) / (self.df_all*N_up)
+        elif lmttyp == 'down':
+            self.supp_down = self.df_down / N_ttl
+            self.conf_down = self.df_down / self.df_all
+            self.lift_down = (N_ttl * self.df_down) / (self.df_all * N_down)
+
+    def __str__(self):
+        frqstr = 'Phrase:{}\nUP: tf={}, df={} \nDOWN: tf={}, df={} \n ALL:     , df={}\n'\
+            .format(self.name, self.tf_up, self.df_up, self.tf_down, self.df_down, self.df_all)
+        ascstr = 'UP: MI={:.3f}, tfidf={:.3f}, support={:.3f}, confidence={:.3f}, lift={:.3f} \nDOWN: MI={:.3f}, tfidf={:.3f}, support={:.3f}, confidence={:.3f}, lift={:.3f}'\
+            .format(self.MI_up, self.tfidf_up, self.supp_up, self.conf_up, self.lift_up, self.MI_down, self.tfidf_down, self.supp_down, self.conf_down, self.lift_down)
+        return frqstr + ascstr
+    def toJSON(self):
+        return json.dumps(self, default=lambda o:o.__dict__, sort_keys=True, indent=4)
+
+def generatePhraseList():
     tf_up = JSON2COUNTER('up', 'tf')
     df_up = JSON2COUNTER('up', 'df')
     tf_down = JSON2COUNTER('down', 'tf')
     df_down = JSON2COUNTER('down', 'df')
     tf_all = JSON2COUNTER('all', 'tf')
     df_all = JSON2COUNTER('all', 'df')
-
     print(df_up)
     print(df_down)
     print(df_all)
 
-    sup_lst, cfd_lst, lft_lst = associations('up', df_up, df_all)
-    print(sup_lst[0:20])
-    print(cfd_lst[0:20])
-    print(lft_lst[0:20])
+    ret_lst = []
+    for key, val in df_all.most_common():
+        phrase = Phrase(name=key,
+                        tf_up= tf_up[key] if key in tf_up else 0,
+                        df_up= df_up[key] if key in df_up else 0,
+                        tf_down= tf_down[key] if key in tf_down else 0,
+                        df_down= df_down[key] if key in df_down else 0
+                        )
+        ret_lst.append(phrase)
+    return ret_lst
+
+def PhraseList2JSON(phraselist):
+    with open('phraselist.json', 'w') as file:
+        json.dump(phraselist, file, default=lambda o:o.__dict__, sort_keys=True, indent=4)
+
+def main():
+    with open('phraselist.json', 'r') as file:
+        x = json.load(file)
+    
+
+
 
 if __name__ == '__main__':
     main()
